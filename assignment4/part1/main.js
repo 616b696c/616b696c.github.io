ddd097ec73e3f4b75b30619ed32ce1100a2833e2
616b696c <139484198+616b696c@users.noreply.github.com>
2025-12-08 22:32:27 -0500

    complete main.js

--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -1,1 +1,47 @@
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
 
+function randomValueFromArray(array) {
+  var random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const events = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and slithered away",
+];
+
+function returnRandomStoryString() {
+  var randomCharacter = randomValueFromArray(characters);
+  var randomPlace = randomValueFromArray(places);
+  var randomEvent = randomValueFromArray(events);
+
+  var storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
+
+  return storyText;
+}
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+  var newStory = returnRandomStoryString();
+
+  if (customName.value !== "") {
+    var name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    var weight = `${Math.round(300 / 14)} stone`;
+    var temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
