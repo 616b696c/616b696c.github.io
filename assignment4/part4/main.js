b8431aaaa55be17b098ddda337aaff3d8c58fd4a
616b696c <139484198+616b696c@users.noreply.github.com>
2025-12-08 23:33:12 -0500

    main.js update

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -83,6 +83,22 @@ class EvilCircle extends Circle
 {
   constructor(x, y) {
     super(x, y, 20, 20, `rgb(0, 0, 0)`, 10);
+    window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
   }
 }
 
